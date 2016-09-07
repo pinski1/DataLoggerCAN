3e2b967b0b1e2f247227d0c3e5cfa755847dd7a4
Robert <pinski1@gmail.com>
2016-09-07 10:47:00 +0100

    Further documentation and some code cleanup.

--- Firmware/DataLoggerCAN/DataLoggerCAN.ino
+++ Firmware/DataLoggerCAN/DataLoggerCAN.ino
@@ -1,7 +1,15 @@
-
-
-/** Datalogger for CAN messages
+/** Datalogger for VESC CAN messages
    @detail Logs CAN messages from a network of connected VESC speed controllers. These messages will be saved locally to SD card (with timestamps) as well as transmitted via XBee to a remote terminal
+   
+   Requires:
+     SD Card
+     XBee RF TRX
+     Teensy 3.2
+   
+   Can't seem to find any concrete information on VESC CANBUS messages. I'm assuming each VESC (once programmed with a unique ID) will transmit status messages including:
+     battery voltage, current, board temperature, motor temperature, motor RPMs, throttle
+   The plan will be to log this to the SD card with timestamps (Teensy 3.2 includes a real time clock)
+   In addition certain information (undecided as to which) will be forwarded via XBee for real time feedback
 
    @status just place holder code for now.
 
@@ -16,24 +24,24 @@
 /** Settings */
 #define USB_COM_SPEED 115200
 #define XBEE_COM_SPEED 115200
-// file formats
-// secret key
+// file names
+#define SECRET_KEY 0xFFFFFFFF // secret key to encrypt telemetry
+#define DEBUG_EN (1) // enable debug messages via USB serial
 
 /** Pin Map */
-#define MCU_2_XBEE D8
-#define XBEE_2_MCU D7
-#define XBEE_RESET D9
-#define CAN_2_MCU D4
-#define MCU_2_CAN D3
-#define LED_ACT D13
-#define LED_STA 0
-#define USB_2_MCU
-#define MCU_2_USB
-#define MOSI D11
-#define MISO  D12
-#define SCK D13
-#define CS_SD     D10
-
+#define MCU_2_XBEE      D8
+#define XBEE_2_MCU      D7
+#define CAN_2_MCU       D4
+#define MCU_2_CAN       D3
+#define LED_ACT         D13
+#define LED_STA         
+#define USB_2_MCU       
+#define MCU_2_USB       
+#define MOSI            D11
+#define MISO            D12
+#define SCK             D13
+#define CS_SD           D10
+#define SW_SD           0
 
 /** Globals */
 // CAN init
@@ -49,14 +57,13 @@ void setup(void) {
 
   Serial.begin(USB_COM_SPEED);
   Serial3.begin(XBEE_COM_SPEED);
-
+  
   xbee.setSerial(Serial3);
-
-  pinMode(XBEE_RESET, OUTPUT);
+  
+  pinMode(SW_SD, INPUT);
   pinMode(LED_STA, OUTPUT);
   pinMode(LED_ACT, OUTPUT);
-
-  digitalWrite(XBEE_RESET, HIGH);
+  
   digitalWrite(LED_STA, LOW);
   digitalWrite(LED_ACT, LOW);
 }
@@ -66,4 +73,3 @@ void loop(void) {
 
 
 }
-
